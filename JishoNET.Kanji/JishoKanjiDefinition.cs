5e4bd7a620095951c02acf113be241bac4b95af4
Myuuiii <mute@mutedevs.nl>
2022-06-27 13:30:16 +0200

    Added comments

--- JishoNET.Kanji/JishoKanjiDefinition.cs
+++ JishoNET.Kanji/JishoKanjiDefinition.cs
@@ -4,10 +4,30 @@ namespace JishoNET.Models
 {
 	public class JishoKanjiDefinition
 	{
+		/// <summary>
+		/// The kanji character.
+		/// </summary>
 		public String Kanji;
+
+		/// <summary>
+		/// The meanings of the kanji.
+		/// </summary>
 		public string[] Meanings { get; set; }
+
+		/// <summary>
+		/// The Kunyomi readings of the kanji.
+		/// </summary>
 		public string[] KunyomiReadings { get; set; }
+
+		/// <summary>
+		/// The Onyomi readings of the kanji.
+		/// </summary>
 		public string[] OnyomiReadings { get; set; }
+
+		/// <summary>
+		/// The number of strokes of the kanji.
+		/// </summary>
+		/// <value></value>
 		public int Strokes { get; set; }
 	}
 }
